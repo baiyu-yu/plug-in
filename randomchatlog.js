31ba096f4f5f750a837c526ec9fd5254aedfcc8f
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2024-09-10 18:42:05 +0800

    Update randomchatlog.js

--- randomchatlog.js
+++ randomchatlog.js
@@ -2,7 +2,7 @@
 // @name         Random Chat Logger
 // @author       白鱼
 // @version      1.0.1
-// @description  随机记录群友发言并且储存在数据库里，然后随机放出一条的插件。使用.chatlog on/off控制开启关闭，可以在配置项配置一些东西。数据库时间太长会很大，请每隔一段时间到\data\default\extensions删除randomChatLogger文件夹（要先禁用插件）。第一条会很快发出来，后面的就正常了，表情包图片可能过期会出现消息无法显示之类的，不管了。
+// @description  随机记录群友发言并且储存在数据库里，然后随机放出一条的插件。使用.chatlog on/off控制开启关闭，可以在配置项配置一些东西。记录时间太长队列会很大，请计时重载插件清空（TODO：指令清空）。第一条会很快发出来，后面的就正常了，表情包图片可能过期会出现消息无法显示之类的，不管了。
 // @timestamp    1724394115
 // @license      MIT
 // @homepageURL  https://github.com/sealdice/javascript
