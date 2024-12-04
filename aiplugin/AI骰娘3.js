4b82c12bd1768cc99d8108e33e33e2831945540f
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2024-12-04 17:34:58 +0800

    Update AI骰娘3.js

--- aiplugin/AI骰娘3.js
+++ aiplugin/AI骰娘3.js
@@ -2,7 +2,7 @@
 // @name         AI Plugin3
 // @author       错误、白鱼
 // @version      3.1.1
-// @description  适用于大部分OpenAI API兼容格式AI的模型插件，测试环境为 Deepseek AI (https://platform.deepseek.com/)，用于与 AI 进行对话，并根据特定关键词触发回复。使用.AI help查看使用方法。具体配置查看插件配置项。 
+// @description  适用于大部分OpenAI API兼容格式AI的模型插件，测试环境为 Deepseek AI (https://platform.deepseek.com/)，用于与 AI 进行对话，并根据特定关键词触发回复。使用.AI help查看使用方法。具体配置查看插件配置项。
 // @timestamp    1721822416
 // @license      MIT
 // @homepageURL  https://github.com/baiyu-yu/plug-in
