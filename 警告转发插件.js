bc1a898511fcffb565115c28f3ae8dddd4ef26f3
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2025-01-22 21:57:31 +0800

    Update 警告转发插件.js

--- 警告转发插件.js
+++ 警告转发插件.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         警告转发插件
-// @description  警告消息转发插件，转发[被禁言]、[被踢出]、[黑名单等级提升]等消息到指定群或私聊。在插件配置项修改相关设置。不要尝试去掉前缀，测试机因为死循环曾创造了一秒发送上百条消息的战绩。
-// @version      1.0.0
+// @description  警告消息转发插件，转发[被禁言]、[被踢出]、[黑名单等级提升]等消息到指定群或私聊。在插件配置项修改相关设置。
+// @version      1.0.1
 // @license      MIT
 // @author       白鱼
 // @timestamp    1730481203
@@ -12,7 +12,7 @@
 // ==/UserScript==
 
 if (!seal.ext.find("消息转发插件")) {
-    const ext = seal.ext.new("消息转发插件", "白鱼", "1.0.0");
+    const ext = seal.ext.new("消息转发插件", "白鱼", "1.0.1");
 
     // 注册配置项
     seal.ext.register(ext);
@@ -21,41 +21,41 @@ if (!seal.ext.find("消息转发插件")) {
     seal.ext.registerTemplateConfig(ext, "转发群号", ["QQ-Group:333333", "QQ-Group:444444"], "配置消息转发的目标群号");
     seal.ext.registerTemplateConfig(ext, "转发私聊QQ", ["QQ:555555", "QQ:666666"], "配置消息转发的目标私聊QQ");
 
-async function sendMessage(groupId, userId, text) {
-    let eps = seal.getEndPoints();
-    for (let i = 0; i < eps.length; i++) {
-        const ep = eps[i];
+    // 动态创建 ctx 和 msg，并发送消息
+    function sendMessage(ep, groupId, userId, text) {
         const msg = seal.newMessage();
         msg.messageType = groupId ? "group" : "private";
         msg.groupId = groupId;
-        msg.guildId = "";
         msg.sender.userId = userId;
+
         const newCtx = seal.createTempCtx(ep, msg);
-        await seal.replyToSender(newCtx, msg, text);
+        
+        seal.replyToSender(newCtx, msg, text);
     }
-}
 
-    async function processMessage(ctx, msg) {
+    // 监听消息事件
+    ext.onNotCommandReceived = (ctx, msg) => {
 
-        // 以指定格式前缀匹配
-        const mutePattern = /^被禁言: 在群组<(.+?)>\((\d+)\)中被禁言，时长(\d+)秒，操作者:<(.+?)>\((\d+)\)/;
-        const kickPattern = /^被踢出群: 在QQ群组<(.+?)>\((\d+)\)中被踢出，操作者:<(.+?)>\((\d+)\)/;
-        const blacklistPattern = /^黑名单等级提升: \[(.+?)\] <(.+?)>\(QQ:(\d+)\) 原因:(.+)/;
+        const mutePattern = /被禁言: 在群组<(.+?)>\((\d+)\)中被禁言，时长(\d+)秒，操作者:<(.+?)>\((\d+)\)/;
+        const kickPattern = /被踢出群: 在QQ群组<(.+?)>\((\d+)\)中被踢出，操作者:<(.+?)>\((\d+)\)/;
+        const blacklistPattern = /黑名单等级提升: \[(.+?)\] <(.+?)>\(QQ:(\d+)\) 原因:(.+)/;
 
         let match;
         let forwardMessage = "";
-        let forwardMessagefrom = "";
 
-        const patterns = [mutePattern, kickPattern, blacklistPattern];
-
+        const patterns = [
+            mutePattern,
+            kickPattern,
+            blacklistPattern
+        ];
+        
         for (const pattern of patterns) {
             if ((match = msg.message.match(pattern))) {
-                forwardMessage = `监听到警告信息：${msg.message}`;//要是敢去掉这个前缀，会发生很有意思的事情哦
-                forwardMessagefrom = `来源：${ctx.player.name} (ID: ${ctx.player.userId})`;
+                forwardMessage = `${msg.message}`;
+                forwardMessageFrom = `来源：${ctx.player.name} (ID: ${ctx.player.userId})`
                 break;
             }
         }
-        if (!forwardMessage) return;
 
         // 检查是否在允许的群或私聊中接收消息
         const allowedGroups = seal.ext.getTemplateConfig(ext, "接收群号");
@@ -66,34 +66,31 @@ async function sendMessage(groupId, userId, text) {
 
         if (!isGroupAllowed && !isPrivateAllowed) return;
 
-        // 转发到目标群和私聊
+        // 匹配到消息类型并允许转发后，执行转发
         const forwardGroups = seal.ext.getTemplateConfig(ext, "转发群号");
         const forwardPrivateQQs = seal.ext.getTemplateConfig(ext, "转发私聊QQ");
 
-        // 向目标群发送消息
-        for (const groupId of forwardGroups) {
-            await sendMessage(groupId, "QQ:114514", forwardMessage);
-            await sendMessage(groupId, "QQ:114514", forwardMessagefrom);
+        const endPoints = seal.getEndPoints();
+        if (endPoints.length === 0) {
+            console.log('未找到任何有效的登录号信息');
+            return;
         }
+        const ep = endPoints[0];  // 选择第一个有效端点作为消息发送的依据
 
-        // 向目标私聊发送消息
-        for (const userId of forwardPrivateQQs) {
-            await sendMessage("", userId, forwardMessage);
-            await sendMessage("", userId, forwardMessagefrom);
+        if (forwardMessage) {
+            // 向目标群发送消息
+            forwardGroups.forEach(groupId => {
+                sendMessage(ep, groupId, "", forwardMessage);
+                sendMessage(ep, groupId, "", forwardMessageFrom);
+            });
+
+            // 向目标私聊发送消息
+            forwardPrivateQQs.forEach(userId => {
+                sendMessage(ep, "", userId, forwardMessage);
+                sendMessage(ep, "", userId, forwardMessageFrom);
+            });
         }
-    }
-
-    // 异步监听消息事件
-    ext.onNotCommandReceived = async (ctx, msg) => {
-        await processMessage(ctx, msg);
-    };
-
-    // 异步监听自身发出的消息
-    ext.onMessageSend = async (ctx, msg) => {
-        await processMessage(ctx, msg);
     };
 }
-
-
 
 
