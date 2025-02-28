20aadf9d07d47d1897abc76eff164a7d9560f3bf
error2913 <2913949387@qq.com>
2025-02-28 21:08:43 +0800

    chore(aiplugin4): 修改版本号

--- aiplugin4/src/index.ts
+++ aiplugin4/src/index.ts
@@ -11,7 +11,7 @@ import { buildSystemMessage } from "./utils/utils_message";
 function main() {
   let ext = seal.ext.find('aiplugin4');
   if (!ext) {
-    ext = seal.ext.new('aiplugin4', 'baiyu&错误', '4.5.6');
+    ext = seal.ext.new('aiplugin4', 'baiyu&错误', '4.5.7');
     seal.ext.register(ext);
   }
 
