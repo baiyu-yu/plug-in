f3b2b57a9c6a7e312efa3a7f1cccbfdaf957d8d5
baiyu-yu <135424680+baiyu-yu@users.noreply.github.com>
2025-03-06 21:13:48 +0800

    音乐卡片tool和README更新

--- aiplugin4/src/tool/tool.ts
+++ aiplugin4/src/tool/tool.ts
@@ -25,6 +25,7 @@ import { registerGetContext } from "./tool_get_context"
 import { registerGetGroupMemberList, registerGetList } from "./tool_get_list"
 import { registerSearchChat, registerSearchCommonGroup } from "./tool_search_chat"
 import { registerSetTriggerCondition } from "./tool_set_trigger_condition"
+import { registerMusicCard } from "./tool_music"
 
 export interface ToolInfo {
     type: "function",
@@ -173,6 +174,7 @@ export class ToolManager {
         registerSearchChat();
         registerSearchCommonGroup();
         registerSetTriggerCondition();
+        registerMusicCard();
     }
 
     /**
